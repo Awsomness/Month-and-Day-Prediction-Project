1afecf20c0936e8e483a12d9c4ad099959dcf0a0
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-12-03 09:48:13 -0500

    Added do while loop

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -8,30 +8,49 @@ public class Main {
         int dayOfWeek;
         int dayOfMonth;
         int month;
-        String month2;
         int year;
         int yearOfCentury;
         int yearZeroBased;
+        int GregorianCalender;
+        String answer4;
+
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-        System.out.print("Monday's child is fair of face,\n" +
-                "Tuesday's child is full of grace,\n" +
-                "Wednesday's child is full of woe,\n" +
-                "Thursday's child has far to go.\n" +
-                "Friday's child is loving and giving,\n" +
-                "Saturday's child works hard for a living,\n" +
-                "But the child born on the Sabbath Day,\n" +
-                "Is fair and wise and good in every way.\n");
+        do {
 
-        System.out.println("What year were you born?");
-        year = keyboard.nextInt();
-        System.out.println("What month were you born?(put in a number)");
-        month = keyboard.nextInt();
-        System.out.println("What month were you born?(Name of month this time)");
-        month2 = keyboard.next();
-        int answer = month(month, year);
-        System.out.println(answer);
+            System.out.print("Monday's child is fair of face,\n" +
+                    "Tuesday's child is full of grace,\n" +
+                    "Wednesday's child is full of woe,\n" +
+                    "Thursday's child has far to go.\n" +
+                    "Friday's child is loving and giving,\n" +
+                    "Saturday's child works hard for a living,\n" +
+                    "But the child born on the Sabbath Day,\n" +
+                    "Is fair and wise and good in every way.\n");
 
+            System.out.println("What year were you born?");
+            year = keyboard.nextInt();
+
+            System.out.println("What month were you born?(put in a number)");
+            month = keyboard.nextInt();
+
+            System.out.println("What is the day that you were born?");
+            dayOfMonth = keyboard.nextInt();
+            int answer = month(month, year);
+            System.out.println("There are " + answer + " days in the month");
+
+
+            int answer2 = GregorianCalender(month, year, dayOfMonth);
+            System.out.println(answer2);
+
+            String answer3 = monthPoem(answer2);
+            System.out.println(answer3);
+            System.out.println("Do you want to continue?");
+            keyboard.nextLine();
+            answer4 = keyboard.nextLine().toLowerCase();
+
+
+
+        } while(answer4.equals("yes"));
     }
 
     public static int month(int month, int year) {
@@ -51,7 +70,7 @@ public class Main {
             case 4:
                 return 30;
             case 2:
-                if (year % 4 == 0 || year % 400 == 0) {
+                if ((year % 100 == 0 && year % 400 == 0) || (!(year % 100 ==0) && year % 4 ==0) ) {
                     return 29;
                 } else {
                     return 28;
@@ -67,36 +86,43 @@ public class Main {
             year--;
 
         }
-        int yearZeroBased;
-        yearZeroBased = (year / 100);
-        int zellerCongurenceThing;
-        zellerCongurenceThing = 13 * (month + 1);
+        int yearZero;
+        yearZero = (year / 100);
+        int GregorianCalender2;
+        GregorianCalender2 = 13 * (month + 1);
         int yearOfCentury;
         yearOfCentury = (year % 100);
-
-
         int dayOfWeek;
-        dayOfWeek = ((dayOfMonth + ((zellerCongurenceThing)/ 5) + yearOfCentury + (yearOfCentury / 4) + ((yearZeroBased) / 4) - (2 * (yearZeroBased))) % 7);
+        dayOfWeek = ((dayOfMonth + ((GregorianCalender2)/ 5) + yearOfCentury + (yearOfCentury / 4) + ((yearZero) / 4) + (5 * (yearZero))) % 7);
         return dayOfWeek;
 
     }
 
     public static String monthPoem(int dayOfWeek) {
 
-        if (dayOfWeek == 1) {
-            return "Your birthday was on Monday \n" + "You have a fair face";
-        } else if (dayOfWeek == 2) {
-            return "Your birthday was on Tuesday \n" + "You are full of grace";
-        } else if (dayOfWeek == 3) {
-            return "Your birthday was on Wendsday \n" + "You are full of woe";
-        } else if (dayOfWeek == 4) {
-            return "Your birthday was on Thursday \n" + "You have far to go";
-        } else if (dayOfWeek == 5) {
-            return "Your birthday was on Friday \n" + "You are loving and giving";
-        } else if (dayOfWeek == 6) {
-            return "Your birthday was on Saturday \n" + "You work hard for a living";
-        } else {
+        switch (dayOfWeek) {
+            case 1:
             return "Your birthday was on Sunday \n" + "You are fair and wise and good in every way.";
+
+            case 2:
+            return "Your birthday was on Monday \n" + "You have a fair face";
+
+            case 4:
+            return "Your birthday was on Wednesday \n" + "You are full of woe";
+
+            case 5:
+            return "Your birthday was on Thursday \n" + "You have far to go";
+
+            case 6:
+            return "Your birthday was on Friday \n" + "You are loving and giving";
+
+            case 0:
+            return "Your birthday was on Saturday \n" + "You work hard for a living";
+
+            default:
+            return "Your birthday was on Tuesday \n" + "You are full of grace";
+        }
+
         }
     }
-}
+
